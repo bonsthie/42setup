ee41c25ecab1ab61edc55814fe14d35e4b0a455e
Barnabe Bonnet <babonnet@3G1.42angouleme.fr>
2023-12-12 19:06:16 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 git clone https://github.com/neovim/neovim
 cd neovim
-make  CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make -j CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
 make install
 cd ..
 
 git clone https://github.com/tmux/tmux.git
 cd tmux
 sh autogen.sh
-./configure && make
+./configure && make -j
 cd ..
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -19,7 +19,8 @@ git clone https://github.com/cacharle/c_formatter_42
 cd c_formatter_42
 pip3 install -e .
 
-cat .zshrc >> ~/.zshrc
+mv ~/.zshrc ~/.zshrc.old
+cp .zshrc ~
 source ~/.zshrc
 
 mkdir ~/.config/
